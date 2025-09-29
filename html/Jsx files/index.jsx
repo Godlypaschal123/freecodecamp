ba9b0e1920c26bc4e7a607fec7435ea5bbb9ee4e
Godlypaschal123 <chigbookpalaugo@gmail.com>
2025-09-29 10:20:52 +0100

    Build-a-reuseable-filter

---
+++ html/Jsx files/index.jsx
@@ -0,0 +1,26 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <ul>
+                <li>About Us</li>
+                <li>Careers</li>
+            </ul>
+            <ul>
+                <li>Support</li>
+                <li>Contact</li>
+            </ul>
+            <ul>
+                <li>Blog</li>
+                <li>Press</li>
+            </ul>
+
+            <p>© 2025 Chigbo Inc. All rights reserved.</p>
+
+            <div>
+                <a href="#">🌐</a>
+                <a href="#">📧</a>
+                <a href="#">📱</a>
+            </div>
+        </footer>
+    );
+};
