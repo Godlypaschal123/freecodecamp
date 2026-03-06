d7a43b943b0eea7d1c132ee6951eba50958beea6
Godlypaschal123 <chigbookpalaugo@gmail.com>
2026-03-06 10:03:35 +0100

    Create aa deep flattening tool

---
+++ javascript/Create a Deep Flattening tool/Flatteningtool.js
@@ -0,0 +1,16 @@
+function steamrollArray(arr) {
+    const result = [];
+
+    function flatten(element) {
+        if (Array.isArray(element)) {
+            for (let i = 0; i < element.length; i++) {
+                flatten(element[i]);
+            }
+        } else {
+            result.push(element);
+        }
+    }
+
+    flatten(arr);
+    return result;
+}
