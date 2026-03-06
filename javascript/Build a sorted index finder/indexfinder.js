0f7d8a8b64d9fa5e6bf933f6ff080272ae9158f8
Godlypaschal123 <chigbookpalaugo@gmail.com>
2026-03-06 10:02:08 +0100

    Build a sorted index finder

---
+++ javascript/Build a sorted index finder/indexfinder.js
@@ -0,0 +1,10 @@
+function getIndexToIns(arr, num) {
+
+    arr.sort((a, b) => a - b);
+
+
+    const index = arr.findIndex(element => num <= element);
+
+
+    return index === -1 ? arr.length : index;
+}
